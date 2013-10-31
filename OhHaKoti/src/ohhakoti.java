c9fc979bea497f0db7681ce95c4a7ca07d7becf0
peteukko <peter.ukkonen@helsinki.fi>
2013-10-31 20:33:00 +0200

    Update ohhakoti.java

--- OhHaKoti/src/ohhakoti.java
+++ OhHaKoti/src/ohhakoti.java
@@ -14,7 +14,7 @@ public class ohhakoti {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hei!!");
+        System.out.println("Hei miten menee!!");
     }
     
 }
